f3c5c6837687a23977e923129424cb05cf7d681d
kzkaed <tree@scutigera.com>
2015-06-30 13:52:28 -0500

    added to player factory spec

--- src/test/scala/players/PlayerFactorySpec.scala
+++ src/test/scala/players/PlayerFactorySpec.scala
@@ -1,5 +1,31 @@
 package players
 
-class PlayerFactorySpec {
+import base.Game
+import mocks.MockUserInterface
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class PlayerFactorySpec extends FunSpec {
   
+  
+ it("gets a human player") {
+   val engine = new ai.Minimax
+   val userInterface = new ui.TConsole
+   val player1 = PlayerFactory.getPlayer("human", "x", engine, userInterface)   
+   val expected1 = new players.Human("x", userInterface)
+   
+   assert(player1.getClass === expected1.getClass)   
+  }
+ 
+  it("get a computer player"){
+   val engine = new ai.Minimax
+   val userInterface = new ui.TConsole
+   val player2 = PlayerFactory.getPlayer("computer", "o", engine, userInterface) 
+   val expected2 = new players.Computer("o", engine)
+  
+   assert(player2.getClass === expected2.getClass)
+  }
 }
