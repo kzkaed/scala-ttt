1f0de5c7140c538a8941eead2d28ea76ae81ef09
kzkaed <tree@scutigera.com>
2015-07-01 09:41:40 -0500

    refactored

--- src/main/scala/base/Game.scala
+++ src/main/scala/base/Game.scala
@@ -36,17 +36,15 @@ class Game(var board: Array[String]) extends GameRules {
    }
    
    def isAcross(): Boolean = {
-     val row1 = (List(board(0), board(1),board(2)).distinct).size == 1
-     val row2 = (List(board(3), board(4),board(5)).distinct).size == 1
-     val row3 = (List(board(6), board(7),board(8)).distinct).size == 1
-     row1 || row2 || row3
+     (List(board(0), board(1),board(2)).distinct).size == 1 ||
+     (List(board(3), board(4),board(5)).distinct).size == 1 || 
+     (List(board(6), board(7),board(8)).distinct).size == 1
    }
    
    def isDown(): Boolean = {
-    val col1 = (List(board(0), board(3),board(6)).distinct).size == 1
-    val col2 = (List(board(1), board(4),board(7)).distinct).size == 1
-    val col3 = (List(board(2), board(5),board(8)).distinct).size == 1
-    col1 || col2 || col3
+     (List(board(0), board(3),board(6)).distinct).size == 1 ||
+     (List(board(1), board(4),board(7)).distinct).size == 1 ||
+     (List(board(2), board(5),board(8)).distinct).size == 1
    }
    
    def isDiagonal(): Boolean = {
