7b55eccc07ce3e28fbcff6e4280b73d32edddcba
kzkaed <tree@scutigera.com>
2015-07-01 20:43:16 -0500

    runner refactor

--- src/main/scala/base/Runner.scala
+++ src/main/scala/base/Runner.scala
@@ -2,7 +2,7 @@ package base
 
 import gui._
 import players._
-
+import scala.util.control.Breaks._
 
 class Runner(val game: GameRules, 
              val userInterface: UserInterface, 
@@ -10,25 +10,39 @@ class Runner(val game: GameRules,
              val player2: Player){
   
   def run() : Unit = {
+    startGame
+    while (!game.isEnd) play 
+    endGame
+  }
+  
+  def startGame(): Unit = {
     userInterface.prepare()
-    userInterface.displayBoard(game.getCurrentBoard())
-    
-    while(!game.isEnd){   
-      var moveSelection = player1.selectMove(game)
-      game.takeTurn(moveSelection, player1.getPlayerMark())
-      var board = game.getCurrentBoard()
-      userInterface.displayBoard(board)
-      
-      println(game.isWin)
-      
-      moveSelection = player2.selectMove(game)
-      game.takeTurn(moveSelection, player2.getPlayerMark())
-      board = game.getCurrentBoard()
-      userInterface.displayBoard(board)
-     }
-    userInterface.goodbye()  
+    userInterface.displayBoard(game.getCurrentBoard()) 
   }
  
+  def play(): Unit =  {
+    firstPlay
+    if (!game.isWin && !game.isDraw()) secondPlay  
+  }
+ 
+  def firstPlay(): Unit = {
+    game.takeTurn(player1.selectMove(game), player1.getPlayerMark())
+    displayCurrentBoard
+  }
+    
+  def secondPlay(): Unit = {
+    game.takeTurn(player2.selectMove(game), player2.getPlayerMark())
+    displayCurrentBoard   
+  }
+  
+  def displayCurrentBoard(): Unit = {
+    userInterface.displayBoard(game.getCurrentBoard())
+  }
+ 
+  def endGame(): Unit = {
+    userInterface.goodbye()
+  }
+    
   
   
 }
--- src/test/scala/base/RunnerSpec.scala
+++ src/test/scala/base/RunnerSpec.scala
@@ -1,7 +1,7 @@
 package base
 
 import mocks._
-import ui._
+import gui._
 import players._
 import org.scalatest._
 import org.junit.runner.RunWith
@@ -15,65 +15,60 @@ class RunnerSpec extends FunSpec {
   val player1 = new MockPlayer
   val player2 = new MockPlayer
   val runner = new Runner(game, userInterface, player1, player2)
+
+  it("has a game") {
+    assert(runner.game == game)  
+  }
   
+  it("has a user interface") {
+    assert(runner.userInterface == userInterface)
+  }
   
-    it("has a game") {
-      assert(runner.game == game)  
-    }
+  it("has players") {
+    assert(runner.player1 == player1)
+    assert(runner.player2 == player2)
+  }
+  
+  it("displays game introduction with user interface"){
+    runner.run()
+    assert(userInterface.isPrepareCalled() == true)
+  }
+  
+  it("displays the board with the user interface"){
+    runner.run()
+    assert(userInterface.isDisplayBoardCalled() == true)
+  }
+  
+  it("receives player1 move from user interface"){
+    val move = "1"
+    player1.moves = move :: player1.moves
     
-    it("has a user interface") {
-      assert(runner.userInterface == userInterface)
-    }
+    runner.run()
     
-    it("has players") {
-      assert(runner.player1 == player1)
-      assert(runner.player2 == player2)
-    }
+    assert(player1.selectMove(game) == "1")   
+    assert(player1.isSelectMoveCalled() == true)
+  }
+  
+  it ("receives player2 move from user inteface") {
+    val move = "2"
+    player2.moves = move :: player2.moves
     
-    it("displays game introduction with user interface"){
-      runner.run()
-      assert(userInterface.isPrepareCalled() == true)
-    }
+    runner.run()
     
-    it("displays the board with the user interface"){
-      runner.run()
-      assert(userInterface.isDisplayBoardCalled() == true)
-    }
-    
-    it("receives player1 move from user interface"){
-      val move = "1"
-      player1.moves = move :: player1.moves
-      
-      runner.run()
-      
-      assert(player1.selectMove(game) == "1")   
-      assert(player1.isSelectMoveCalled() == true)
-    }
-    
-    it ("receives player2 move from user inteface") {
-      val move = "2"
-      player2.moves = move :: player2.moves
-      
-      runner.run()
-      
-      assert(player2.selectMove(game) == "2")
-      assert(player2.isSelectMoveCalled() == true)
-    }
-     
-    it ("sends move and player to game and gets a board") {
-     val moveSelection = "1"
-     val playerMark = "x"
-     val fakeBoard = Array("x","2","3","4","5","6","7","8","9")
-     game.boards = fakeBoard :: game.boards
-     val boardResult = Array("x","2","3","4","5","6","7","8","9")
-     
-     runner.run()
-     assert(game.takeTurn(moveSelection, playerMark) === boardResult)
-     assert(game.isTakeTurnCalled() == true)
-    }
-    
-    
-    
-    
+    assert(player2.selectMove(game) == "2")
+    assert(player2.isSelectMoveCalled() == true)
+  }
+   
+  it ("sends move and player to game and gets a board") {
+   val moveSelection = "1"
+   val playerMark = "x"
+   val fakeBoard = Array("x","2","3","4","5","6","7","8","9")
+   game.boards = fakeBoard :: game.boards
+   val boardResult = Array("x","2","3","4","5","6","7","8","9")
+   
+   runner.run()
+   assert(game.takeTurn(moveSelection, playerMark) === boardResult)
+   assert(game.isTakeTurnCalled() == true)
+  }  
 }
   
