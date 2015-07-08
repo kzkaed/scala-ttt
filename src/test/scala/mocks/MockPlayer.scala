a9e7e0d5e699279f1d26d4285e7d915d62f7ac6d
kzkaed <tree@scutigera.com>
2015-07-08 09:43:53 -0500

    eliminated unused mock method

--- src/test/scala/mocks/MockPlayer.scala
+++ src/test/scala/mocks/MockPlayer.scala
@@ -6,25 +6,15 @@ class MockPlayer extends Player{
   var moves = List[String]()
   var selectMoveCalled = false
   var playerMark = "x"
-  var getPlayerMarkCalled = false
   
    override def selectMove(game: GameRules): String = {
     selectMoveCalled = true 
     if (moves.isEmpty) "" else moves.head
   }
-  
-  def getPlayerMark(): String = {
-    getPlayerMarkCalled = true
-    playerMark
-  }
- 
-  
+
   //expectations
   def isSelectMoveCalled(): Boolean = {
     selectMoveCalled
   }
-  
-  def isGetPlayerMarkCalled() : Boolean = {
-    getPlayerMarkCalled 
-  }
+ 
 }
